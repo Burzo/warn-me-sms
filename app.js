48bfe2421c4526042022a33097a4e3bb1a3c21da
unknown <miha.drofenik92@gmail.com>
2018-11-04 19:01:55 +0100

    added .crt and https test routes

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require("./events.js")
 require("./config/config.js")
 
 const express = require("express")
+const path = require("path")
 const fs = require("fs").promises
+const fs2 = require("fs")
 var cron = require('node-cron');
 const async2 = require("async")
 const moment = require("moment")
@@ -14,12 +16,27 @@ const {myEE} = require("./events")
 const send = require("./src/send-sms.js")
 const asyncForEach = require("./utils/helpers.js")
 const sendToMore = require("./src/send-to-more.js")
+const https = require("https")
 
 const app = express()
 
-// TEST IF LOADING
-// myEE.on("onUrnikLoad", (res) =>  console.log())
-// myEE.on("onNumbersLoad", (res) =>  console.log())
+app.use(express.static(path.join(__dirname, "/public")))
+
+
+
+app.get("/", (req, res) => {
+    res.send("index.html")
+})
+
+app.get("/test", (req, res) => {
+    res.send("index.html")
+})
+
+
+
+
+
+
 
 cron.schedule(" 0 5 * * * ", () => {
     sendToMore()
@@ -42,7 +59,15 @@ cron.schedule("0 4 * * *", async () => {
 })
 
 
-app.listen(process.env.PORT, ()=> {
+// app.listen(process.env.PORT, ()=> {
+//     fs.appendFile(process.env.PATHTOLOGS+"/server-logs.txt", `${moment().format("D.M - H:m:s")}: LISTENING ON PORT ${process.env.PORT}\n`)
+//     console.log(`${moment().format("D.M - H:m:s")}: LISTENING ON PORT ${process.env.PORT}`)
+// })
+
+https.createServer({
+    key: fs2.readFileSync("./config/crt/privateKey.key"),
+    cert: fs2.readFileSync("./config/crt/certificate.crt")
+}, app).listen(2000, () => {
     fs.appendFile(process.env.PATHTOLOGS+"/server-logs.txt", `${moment().format("D.M - H:m:s")}: LISTENING ON PORT ${process.env.PORT}\n`)
     console.log(`${moment().format("D.M - H:m:s")}: LISTENING ON PORT ${process.env.PORT}`)
 })
